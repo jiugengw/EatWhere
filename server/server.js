0334b763a3e9a14688883a89052e310dd1024f42
jiugeng <130973940+jiugengw@users.noreply.github.com>
2025-06-05 11:20:49 +0800

    Finish routing for user

--- server/server.js
+++ server/server.js
@@ -1,19 +1,23 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const user = require('./models/userModel');
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-const app = express();
+import userRoutes from './routes/userRoutes.js';
+import groupRoutes from './routes/groupRoutes.js';
 
 dotenv.config({ path: './.env' });
+
+const app = express();
 
 const db = process.env.DATABASE.replace(
   '<db_password>',
   process.env.DATABASE_PASSWORD
 );
-
-mongoose.connect(db);
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error('MongoDB not connected', err));
 
 const corsOptions = {
   origin: ['http://localhost:5173'],
@@ -21,19 +25,11 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 
-// app.post('/api/users', async (req, res) => {
-//   try {
-//     const { name, password, preferences, avoid } = req.body;
-//     const newUser = new user({ name, password, preferences, avoid });
-//     await newUser.save();
-//     res.status(201).json({ message: 'User created', user: newUser });
-//   } catch (err) {
-//     console.error('Error creating user:', err);
-//     res.status(500).json({ error: 'Failed to create user' });
-//   }
-// });
+app.use('/api/users', userRoutes);
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
+
+
