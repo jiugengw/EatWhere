1cd5412b375926c702e978ab5d1e4fe010759207
nameless1130 <nathanielteo1130@gmail.com>
2025-06-09 22:00:15 +0800

    added token generation and password hash

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,16 +1,40 @@
 import User from './../models/userModel.js';
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+
 
 export const createUser = async (req, res) => {
   try {
-    const newUser = await User.create(req.body);
-
+    const {name,password,preferences,avoid,group,history} = req.body;
+    const hashpassword = await bcrypt.hash
+    (password,10);
+    
+    const newUser = await User.create({
+      name,
+      password:hashpassword,
+      preferences,
+      avoid,
+      group,
+      history
+  });
+  const token = jwt.sign(
+      {name:newUser.name,
+        userId:newUser._id,
+        preferences: newUser.preferences
+      },
+      process.env.JWT_TOKEN,
+      {expiresIn:'1h'}
+    )
+    
     res.status(201).json({
       status: 'success',
+      token,
       data: {
         user: newUser,
       },
     });
   } catch (err) {
+    console.log(err)
     res.status(400).json({
       status: 'fail',
       message: err,
@@ -18,17 +42,39 @@ export const createUser = async (req, res) => {
   }
 };
 
+
 export const getUser = async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
 
+    if (!user){
+      return res.status(404).json({
+        status:"fail",
+        message:'user not found'
+      })
+    }
+    const token = jwt.sign(
+      {name:user.name,
+        userid:user._id,
+        preferences:user.preferences
+      },
+      process.env.JWT_TOKEN,
+      {expiresIn:'1h'}
+    )
     res.status(200).json({
       status: 'success',
       data: {
         user,
+        token
       },
     });
   } catch (err) {
+    if (err.name === 'CastError'){
+      return res.status(404).json({
+        status:"fail",
+        message:'user not found'
+      })
+    }
     res.status(500).json({
       status: 'fail',
       message: err,
@@ -64,6 +110,7 @@ export const deleteUser = async (req, res) => {
     res.status(204).json({
       status: 'success',
       data: null,
+      message:"successfully deleted user"
     });
   } catch (err) {
     res.status(404).json({
@@ -72,3 +119,31 @@ export const deleteUser = async (req, res) => {
     });
   }
 };
+
+//controllers below for testing
+
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    
+    res.status(200).json({ status: 'success', data: users });
+
+  } catch (err) {
+    res.status(500).json({ status: 'fail', message: err.message });
+  }
+};
+
+export const generateToken = async(req,res)=>{
+  try{
+    const token = jwt.sign({
+      name:'test'
+    },process.env.JWT_TOKEN,
+  {expiresIn:'1h'})
+  res.status(201).json({
+    status:'success',
+    message:'add the following below as key:value for header on postman',
+    Authorization:`Bearer: ${token}`})
+  }catch(err){
+    res.status(400).json({status:'fail',message:err})
+  }
+}
