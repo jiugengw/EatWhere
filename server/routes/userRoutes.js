fa5940144167a8984bcee53605e1df957e4e7afd
nameless1130 <nathanielteo1130@gmail.com>
2025-06-09 22:01:56 +0800

    added testing routes to test token generation

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,16 +1,32 @@
 import express from 'express';
 import * as userController from './../controllers/userController.js';
+import {protect} from './../middleware/auth.js';
 
 const router = express.Router();
 
+//below is to generate a token
+router
+  .route('/generatetoken')
+  .post(userController.generateToken);
+  
 router
   .route('/')
   .post(userController.createUser);
+
+// comment this out if dont have a jwt token
+router
+  .use(protect);
+
+//this is here temporarily
+router
+  .route('/check')
+  .post(userController.getAllUsers);
 
 router
   .route('/:id')
   .get(userController.getUser)
   .patch(userController.updateUser)
   .delete(userController.deleteUser);
+
 
 export default router;
