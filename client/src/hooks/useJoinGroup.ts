03edc4e63fe64e965a126adefb0e28674d45656c
jiugeng <130973940+jiugengw@users.noreply.github.com>
2025-07-03 09:20:14 +0800

    WIP: temp changes to useJoinGroup

--- client/src/hooks/useJoinGroup.ts
+++ client/src/hooks/useJoinGroup.ts
@@ -1,16 +1,16 @@
 import { useMutation } from '@tanstack/react-query';
 import useAxiosPrivate from './useAxiosPrivate';
-import type { JoinGroupInput } from '@/shared/schemas/JoinGroupSchema';
 import { useNavigate } from '@tanstack/react-router';
 import { showNotification } from '@mantine/notifications';
+import type { AxiosError } from 'axios';
 
 export const useJoinGroup = () => {
   const axiosPrivate = useAxiosPrivate();
   const navigate = useNavigate();
 
   return useMutation({
-    mutationFn: async (data: JoinGroupInput) => {
-      const res = await axiosPrivate.post('/groups/join', data);
+    mutationFn: async (code: string) => {
+      const res = await axiosPrivate.patch(`/groups/${code}/join`);
       return res.data;
     },
     onSuccess: (data) => {
@@ -19,12 +19,14 @@ export const useJoinGroup = () => {
         message: `Joined group: ${data.group.name}`,
         color: 'green',
       });
-      navigate({ to: '/groups' });
+      navigate({ to: '/group' });
     },
-    onError: (error: any) => {
+    onError: (error: unknown) => {
+      const err = error as AxiosError<{ message: string }>;
+
       showNotification({
         title: 'Error',
-        message: error?.response?.data?.message || 'Failed to join group',
+        message: err?.response?.data?.message || 'Failed to join group',
         color: 'red',
       });
     },
